33ec9ef3d59605d9fa04d68ea4b567e2eedcd0f9
Leo Cavaille <leo@datadoghq.com>
2019-07-10 16:25:56 +0200

    add audit logs

--- katacodacomleocdd-k8s-monitoring/background.sh
+++ katacodacomleocdd-k8s-monitoring/background.sh
@@ -1,3 +1,14 @@
 git clone https://github.com/LeoCavaille/k8s-workshop
 mv k8s-workshop/assets ./assets
 rm -rf k8s-workshop
+
+# add audit logs to the apiserver
+mkdir -p /etc/kubernetes/audit-policies
+cp assets/audit-logs/policy.yaml /etc/kubernetes/audit-policies/policy.yaml
+
+sed -i '/tls-private-key-file/a \ \ \ \ - --audit-policy-file=/etc/kubernetes/audit-policies/policy.yaml' /etc/kubernetes/manifests/kube-apiserver.yaml
+sed -i '/audit-policy-file/a \ \ \ \ - --audit-log-path=/var/log/kubernetes/apiserver/audit.log' /etc/kubernetes/manifests/kube-apiserver.yaml
+sed -i '/volumes:/a \ \ - {hostPath: {path: /etc/kubernetes/audit-policies, type: DirectoryOrCreate}, name: k8s-audit-policies}' /etc/kubernetes/manifests/kube-apiserver.yaml
+sed -i '/volumeMounts:/a \ \ \ \ - {mountPath: /etc/kubernetes/audit-policies, name: k8s-audit-policies, readOnly: true}' /etc/kubernetes/manifests/kube-apiserver.yaml
+sed -i '/volumes:/a \ \ - {hostPath: {path: /var/log/kubernetes, type: DirectoryOrCreate}, name: k8s-logs}' /etc/kubernetes/manifests/kube-apiserver.yaml
+sed -i '/volumeMounts:/a \ \ \ \ - {mountPath: /var/log/kubernetes, name: k8s-logs}' /etc/kubernetes/manifests/kube-apiserver.yaml
