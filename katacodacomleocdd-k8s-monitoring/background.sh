a65684eff624914dafea7f736677aa0e2b8f5507
Leo Cavaille <leo@datadoghq.com>
2019-07-15 07:48:48 -0400

    fix audit policy setup

--- katacodacomleocdd-k8s-monitoring/background.sh
+++ katacodacomleocdd-k8s-monitoring/background.sh
@@ -12,10 +12,10 @@ grep "audit-policy-file" /etc/kubernetes/manifests/kube-apiserver.yaml || \
 grep "audit-log-path" /etc/kubernetes/manifests/kube-apiserver.yaml || \
 	sed -i '/audit-policy-file/a \ \ \ \ - --audit-log-path=/var/log/kubernetes/apiserver/audit.log' /etc/kubernetes/manifests/kube-apiserver.yaml
 
-grep "/etc/kubernetes/audit-policies" /etc/kubernetes/manifests/kube-apiserver.yaml || \
+grep "path: /etc/kubernetes/audit-policies" /etc/kubernetes/manifests/kube-apiserver.yaml || \
 	sed -i '/volumes:/a \ \ - {hostPath: {path: /etc/kubernetes/audit-policies, type: DirectoryOrCreate}, name: k8s-audit-policies}' /etc/kubernetes/manifests/kube-apiserver.yaml
 
-grep "/etc/kubernetes/audit-policies" /etc/kubernetes/manifests/kube-apiserver.yaml || \
+grep "mountPath: /etc/kubernetes/audit-policies" /etc/kubernetes/manifests/kube-apiserver.yaml || \
 	sed -i '/volumeMounts:/a \ \ \ \ - {mountPath: /etc/kubernetes/audit-policies, name: k8s-audit-policies, readOnly: true}' /etc/kubernetes/manifests/kube-apiserver.yaml
 
 grep "path: /var/log/kubernetes" /etc/kubernetes/manifests/kube-apiserver.yaml || \
